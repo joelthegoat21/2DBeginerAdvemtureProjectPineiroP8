a893ba4865862154cf5691a8b3506cc895252be8
joelthegoat21 <729037@student.dusd.net>
2024-10-11 15:28:44 -0700

    making the 2d adventure beginer
    today i managed to  create a new respository and my project added the assets and  created mr quinteros first video

---
+++ 2DBeginerAdventureP8Pineiro/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+
+     
+        position.x = position.x + 0.1f;
+        transform.position = position;
+    }
+         
+}
