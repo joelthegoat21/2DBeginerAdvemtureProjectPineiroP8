e4db4d1a11eaf8e0271c1d83d27bd7a4833ee4a4
joelthegoat21 <729037@student.dusd.net>
2024-10-16 15:26:06 -0700

    making the project to the laptop
    we got brand new laptops and transfered our project into them so now we can work our project from home

--- 2DBeginerAdventureP8Pineiro/Assets/Scripts/PlayerController.cs
+++ 2DBeginerAdventureP8Pineiro/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,9 +16,9 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 position = transform.position;
 
-     
+
         position.x = position.x + 0.1f;
         transform.position = position;
     }
-         
+    
 }
