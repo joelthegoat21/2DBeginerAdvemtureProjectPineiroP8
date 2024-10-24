29921b6c894b3b26af929f6e4bc6842298f4f6a5
joelthegoat21 <729037@student.dusd.net>
2024-10-24 15:23:32 -0700

    decoratingmymap
    i added new decorations to my world  and  manged to create new  stuff to  my world and also starting  to learn about world  interactions.

--- 2DBeginerAdventureP8Pineiro/Assets/Scripts/PlayerController.cs
+++ 2DBeginerAdventureP8Pineiro/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
@@ -17,8 +18,8 @@ public class PlayerController : MonoBehaviour
          float vertical = Input.GetAxis("Vertical");
      
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical *Time.deltaTime;
 
         transform.position = position;
     }
