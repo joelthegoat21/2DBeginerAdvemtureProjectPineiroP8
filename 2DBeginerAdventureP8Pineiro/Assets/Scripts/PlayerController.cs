c2376f5b2ca1752c2b77121768b984fbf015fd8e
joelthegoat21 <729037@student.dusd.net>
2024-11-05 15:28:09 -0800

    i added a damage zone
    today i managed to make a damage zone in my 2d beginer adventure where i can finally now be able to take damage

--- 2DBeginerAdventureP8Pineiro/Assets/Scripts/PlayerController.cs
+++ 2DBeginerAdventureP8Pineiro/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,19 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2;
     public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
-    float verticcal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +29,21 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
-        float horizontal = Input.GetAxis("Horizontal");
-       float   vertical = Input.GetAxis("Vertical");
-     
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+
+        }
+    
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical *Time.deltaTime;
@@ -34,6 +52,15 @@ public class PlayerController : MonoBehaviour
     }
   public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
